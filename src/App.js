a85442c4a8d38d3a19d9d03316a5858b70031b99
krishna-creator <krishnachowdary.dk@gmail.com>
2021-01-15 14:59:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import data from './data';
+import React,{useState,useEffect} from 'react';
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [person,setData]=useState(data);
+  const [index,setIndex]=useState(0);
+  useEffect(()=>{
+      if(index<0){
+        setIndex(person.length-1);
+      }
+      if(index>person.length-1){
+        setIndex(0);
+      }
+    },[index]);
+    useEffect(()=>{
+      var s=setTimeout(
+        ()=>{
+          setIndex(index+1);
+          },3000
+        );
+        return ()=>{
+          clearInterval(s);
+        }
+    })
+    return (
+    <>
+    <h1 className="jumbotron text-center">Slider</h1>
+    <div className="container d-flex section-center">
+      {person.map((people,ind)=>{
+        let position="nextSlide";
+        if(index===ind){
+          position="activeSlide";
+        }
+        if(ind===index-1 || (index===0 && ind===person.length-1)){
+          position="lastSlide";
+        }
+        const {id,image,name,quote}=people;
+        return(
+          <article className={position} key={id}>
+            <h1>{name}</h1>
+            <img className="person-img" src={image}></img>
+            <h4><q>{quote}</q></h4>
+          </article>
+        )
+      })}
+      <button className="prev" onClick={() => setIndex(index - 1)}>
+          <FiChevronLeft />
+        </button>
+        <button className="next" onClick={() => setIndex(index + 1)}>
+          <FiChevronRight />
+        </button>
     </div>
+    </>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,27 @@
+const person=[
+    {
+        id:1,
+        image:"./images/img1.jpg",
+        name:"Iron Man",
+        quote:"Is it better to be feared or respected? I say, is it too much to ask for both?",
+    },
+    {
+        id:2,
+        image:"./images/img1.jpg",
+        name:"Captain America",
+        quote:"Is it better to be feared or respected? I say, is it too much to ask for both?",
+    },
+    {
+        id:3,
+        image:"./images/img1.jpg",
+        name:"Thor",
+        quote:"Is it better to be feared or respected? I say, is it too much to ask for both?",
+    },
+    {
+        id:4,
+        image:"./images/img1.jpg",
+        name:"Hulk",
+        quote:"Is it better to be feared or respected? I say, is it too much to ask for both?",
+    }
+];
+export default person;
